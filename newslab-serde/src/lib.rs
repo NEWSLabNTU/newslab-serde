6c802ecc903de1ea090af8d524ceb312c1af3b7b
aeon <jerry73204@gmail.com>
2023-09-13 12:51:58 +0800

    Rename newslab-serde-range to newslab-serde-common

--- newslab-serde/src/lib.rs
+++ newslab-serde/src/lib.rs
@@ -1,7 +1,7 @@
+pub use newslab_serde_common as common;
 pub use newslab_serde_cv as cv;
 #[cfg(feature = "with-measurements")]
 pub use newslab_serde_measurements as measurements;
 #[cfg(feature = "with-nalgebra")]
 pub use newslab_serde_nalgebra as nalgebra;
 pub use newslab_serde_num as num;
-pub use newslab_serde_range as range;
